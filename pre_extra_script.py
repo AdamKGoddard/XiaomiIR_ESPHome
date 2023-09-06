423710653f97f27e4cd44e974fd2203ac82c001c
Adam Goddard <53322762+AdamKGoddard@users.noreply.github.com>
2023-09-06 13:14:41 +1000

    Update pre_extra_script.py
    Fixed for ESPHome python file traversing

--- pre_extra_script.py
+++ pre_extra_script.py
@@ -1,2 +1,2 @@
-from shutil import copyfile
-copyfile("../remote_transmitter_esp32.cpp", "./src/esphome/components/remote_transmitter/remote_transmitter_esp32.cpp")
+from shutil import copy2
+copy2("/config/esphome/remote_transmitter_esp32.cpp", "./src/esphome/components/remote_transmitter/remote_transmitter_esp32.cpp")
